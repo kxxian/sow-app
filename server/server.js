a2b8d191f573de237e72235a6ef46d46e00a8a03
kxxian <kristianryanbulos@gmail.com>
2026-02-04 15:15:15 +0800

    setup backend server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Server is live!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
