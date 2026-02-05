b798079de96377676320b34c195769b46c4f9fdc
kxxian <kristianryanbulos@gmail.com>
2026-02-05 18:50:08 +0800

    added toaster and router for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,19 @@ import Login from "./pages/Login";
 import OurCustomers from "./pages/OurCustomers";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/order" element={<Order />} />
         <Route path="/our-customers" element={<OurCustomers />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
