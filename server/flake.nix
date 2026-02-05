c0e46973fcebcbf2a401e676d8ec5eb866d2d92f
kxxian <kristianryanbulos@gmail.com>
2026-02-05 14:23:13 +0800

    added for prisma config for NixOS

---
+++ server/flake.nix
@@ -0,0 +1,32 @@
+## Create a flake.nix file
+{
+  description = "Run 'nix develop' to have a dev shell that has everything this project needs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            nodePackages_latest.pnpm
+            nodePackages_latest.vercel
+            nodePackages_latest.prisma
+            postgresql
+            openssl
+          ];
+          shellHook = ''
+                export PRISMA_QUERY_ENGINE_LIBRARY=${pkgs.prisma-engines}/lib/libquery_engine.node
+                export PRISMA_QUERY_ENGINE_BINARY=${pkgs.prisma-engines}/bin/query-engine
+                export PRISMA_SCHEMA_ENGINE_BINARY=${pkgs.prisma-engines}/bin/schema-engine
+          '';
+        };
+      });
+}
