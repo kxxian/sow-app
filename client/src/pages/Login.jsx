046c34211344b2d0fa1286b0586f4c5a04cde3a7
kxxian <kristianryanbulos@gmail.com>
2026-02-05 18:49:52 +0800

    added login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,9 +4,41 @@ import Footer from "../components/Footer";
 import LoginBackground from "../components/LoginBackground";
 import NavigationHeader from "../components/NavigationHeader";
 import { useNavigate } from "react-router-dom";
+import api from "../axios/axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (field, value) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post("/api/login", {
+        email: formData.email,
+        password: formData.password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+      console.log("Logged in:", res.data);
+      toast.success("Login successful");
+      navigate("/home");
+    } catch (error) {
+      toast.dismissAll();
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error.response?.data || error.message);
+    }
+  };
 
   return (
     <div className="login-container">
@@ -18,7 +50,7 @@ const Login = () => {
       <div className="content">
         <div className="login-content-root">
           <div className="back-login">
-            <form noValidate="" autoComplete="off">
+            <form onSubmit={handleSubmit} noValidate="" autoComplete="off">
               <h2 className="login-heading">Log in</h2>
               <section className="login-section">
                 <div className="login-email">
@@ -27,14 +59,13 @@ const Login = () => {
                       Enter your email address
                     </label>
                   </div>
-                  <input
+                  <InputField
                     className="login-input"
                     type="email"
-                    id="email"
-                    required=""
-                    name="username"
-                    autoComplete="on"
+                    value={formData.email}
                     placeholder="Email address"
+                    onChange={(v) => handleInputChange("email", v)}
+                    required={true}
                   />
                 </div>
                 <span className="email-error-span error-span"></span>
@@ -45,15 +76,15 @@ const Login = () => {
                     </label>
                   </div>
                   <div className="password-input-div">
-                    <input
+                    <InputField
                       className="login-input"
                       type="password"
-                      id="password"
-                      required=""
-                      name="password"
+                      value={formData.password}
                       placeholder="Password"
+                      onChange={(v) => handleInputChange("password", v)}
+                      required={true}
                     />
-                    <img id="show-password-img" src={assets.showPwd} alt="" />
+                    {/*<img id="show-password-img" src={assets.showPwd} alt="" /> */}
                   </div>
                 </div>
                 <span className="password-error-span error-span"></span>
@@ -84,5 +115,24 @@ const Login = () => {
     </div>
   );
 };
+
+/* --- Common Elements --- */
+const InputField = ({
+  value,
+  onChange,
+  className,
+  placeholder,
+  type = "text",
+  required = "false",
+}) => (
+  <input
+    type={type}
+    value={value}
+    onChange={(e) => onChange(e.target.value)}
+    placeholder={placeholder}
+    className={className}
+    required={required}
+  />
+);
 
 export default Login;
