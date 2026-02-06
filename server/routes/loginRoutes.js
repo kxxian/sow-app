fed0ae30317eb8e7a8a74010b255754b6d80ca62
kxxian <kristianryanbulos@gmail.com>
2026-02-06 17:05:28 +0800

    updated login routes

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { userLogin } from "../controllers/loginController.js";
+import { userLogin, userMe } from "../controllers/loginController.js";
+import { requireAuth } from "../middleware/authMiddleware.js";
 
 const loginRouter = express.Router();
 
 loginRouter.post("/", userLogin);
+loginRouter.get("/me", requireAuth, userMe);
 
 export default loginRouter;
