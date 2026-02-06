90506b36c32b8e487a7cfac2d0f18715ea7d9c4f
kxxian <kristianryanbulos@gmail.com>
2026-02-06 17:03:47 +0800

    fetch user saved data on token

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,41 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import api from "../axios/axios";
+import LoginNavbar from "../components/LoginNavbar";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState([]);
+  const token = localStorage.getItem("token");
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/");
-  };
+  // console.log(JSON.parse(atob(token.split(".")[1])));
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const { data } = await api.get("/api/login/me", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        setUserInfo(data.user);
+        console.log(data.user);
+      } catch (error) {
+        toast.error(error?.response?.data?.message || error.message);
+        console.log(`Error ${error}`);
+      }
+    };
+
+    if (!token) {
+      console.log("No token in localStorage!");
+      navigate("/");
+    }
+    fetchUserInfo();
+  }, [navigate, token]);
+
   return (
     <div>
-      <h1>Home page</h1>
-      <button onClick={handleLogout}>Logout</button>
+      <LoginNavbar userInfo={userInfo} />
     </div>
   );
 };
