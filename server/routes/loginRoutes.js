2afe5f954968ef6f82f558bc33527e2dee7b86fe
kxxian <kristianryanbulos@gmail.com>
2026-02-09 17:27:50 +0800

    added two routes for getting and setting languages

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { userLogin, userMe } from "../controllers/loginController.js";
+import {
+  getLang,
+  setLang,
+  userLogin,
+  userMe,
+} from "../controllers/loginController.js";
 import { requireAuth } from "../middleware/authMiddleware.js";
 
 const loginRouter = express.Router();
 
 loginRouter.post("/", userLogin);
+loginRouter.get("/lang", getLang);
+loginRouter.post("/lang", setLang);
 loginRouter.get("/me", requireAuth, userMe);
 
 export default loginRouter;
